eb0e40684f08842f6ad93644e07eabc6b6dbf84e
zacrogersworks <hello@zacrogers.works>
2022-05-31 17:48:37 -0700

    Updated config file to allow netlify build

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,14 @@ module.exports = {
   },
   plugins: ["gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap", {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        "icon": "src/images/icon.png"
-      }
-    }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    "gatsby-plugin-sitemap", 
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     "icon": "src/images/icon.png"
+    //   }
+    // }
+    , "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "images",
