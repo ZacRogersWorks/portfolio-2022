4d03841314ee3b297672cc1ffe11a2a72b25c6b1
zacrogersworks <hello@zacrogers.works>
2022-06-03 11:06:13 -0700

    Updated image query for Project page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,19 +3,21 @@ import React from 'react'
 import Background from '../components/three/Background'
 import Nav from '../components/sections/Nav'
 import Contact from '../components/sections/Contact'
-import {useSiteContext} from '../components/context/SiteContext'
+import { useSiteContext } from '../components/context/SiteContext'
 import '../styles/styles.scss'
 
 const Layout = ({ children }) => {
-  const {section} = useSiteContext()
+  const { section } = useSiteContext()
 
   return (
-    <div className="page-container">
+    <>
       <Background />
-      <Nav />
-      <main>{children}</main>
-      <Contact ref={section.refs.contact}/>
-    </div>
+      <div className="page-container">
+        <Nav />
+        <main>{children}</main>
+        <Contact ref={section.refs.contact} />
+      </div>
+    </>
   )
 }
 
