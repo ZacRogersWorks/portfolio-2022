b14360c444f857a440788a6c56a5af90a6aadde3
zacrogersworks <hello@zacrogers.works>
2022-06-06 15:12:08 -0700

    Updated Layout configuration to avoid strange scrolling behavior on page change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import { SiteContextProvider } from './src/components/context/SiteContext'
+import { AnimatePresence } from 'framer-motion'
 
 export const wrapRootElement = ({ element }) => {
     return (
         <SiteContextProvider>
             {element}
         </SiteContextProvider>
+    )
+}
+
+export const shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition
+}) => {
+    const TRANSITION_DELAY = .3 * 1000 * 2
+
+    if (location.action === "PUSH") {
+        window.setTimeout(() => window.scrollTo(0,0), TRANSITION_DELAY)
+    }
+
+    else {
+        const savedPosition = getSavedScrollPosition(location) || [0,0]
+        window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+    }
+
+    return false
+}
+
+export const wrapPageElement = ({ element }) => {
+    return (
+        <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
     )
 }
--- gatsy-ssr.js
+++ gatsy-ssr.js
@@ -8,3 +8,9 @@ export const wrapRootElement = ({ element }) => {
         </SiteContextProvider>
     )
 }
+
+export const wrapPageElement = ({ element }) => {
+    return (
+        <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+    )
+}
