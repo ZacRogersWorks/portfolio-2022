36114de11e30799d309333ded21cfdc5079845de
zacrogersworks <hello@zacrogers.works>
2022-07-01 15:45:02 -0700

    Added copy to MDX files for projects, adjusted featured image responsiveness

--- src/components/queries/useWorkListQuery.js
+++ src/components/queries/useWorkListQuery.js
@@ -8,6 +8,7 @@ export const useWorkListQuery = () => {
             frontmatter {
               description
               date
+              title
             }
             slug
             id
