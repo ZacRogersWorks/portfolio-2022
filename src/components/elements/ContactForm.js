70d668c46c2a47b04dce3be0ccb8db88f19b57f1
zacrogersworks <hello@zacrogers.works>
2022-06-30 20:50:59 -0700

    Testing contact form bug fix

--- src/components/elements/ContactForm.js
+++ src/components/elements/ContactForm.js
@@ -34,7 +34,7 @@ const ContactForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         setSending(true)
-        const payload = encode({ "form-name": "zacrogers.works", ...formState })
+        const payload = encode({ "form-name": "ZacRogers.Works", ...formState })
 
         fetch("/", {
             method: 'POST',
@@ -65,15 +65,16 @@ const ContactForm = () => {
             initial="initial"
             animate={motionControls}
         >
+            
             <motion.p variants={SECTION_VARIANTS.fadeUpP}>Like what you see? Let's chat.</motion.p>
             <SuccessCard setSent={setSent} sending={sending} sent={sent} />
             <motion.form
                 name="ZacRogers.Works"
-                data-netlify="true"
                 method="POST"
+                data-netlify="true"
                 className="contact-form"
                 data-netlify-honeypot="bot-field"
-                // onSubmit={handleSubmit}
+                onSubmit={handleSubmit}
             >
                 {
                     isError && (
@@ -82,6 +83,7 @@ const ContactForm = () => {
                         </div>
                     )
                 }
+                <input type="hidden" name="form-name" value="ZacRogers.Works" />
                 <motion.div variants={SECTION_VARIANTS.fadeUpElement} className="input-container">
                     <input className="contact-form-input" placeholder=" " type="name" id="name" name="name" onChange={handleChange} value={name}/>
                     <label htmlFor="name">Name</label>
@@ -94,7 +96,7 @@ const ContactForm = () => {
                     <textarea className="contact-form-input" placeholder=" " id="message" name="message" onChange={handleChange} value={message}/>
                     <label htmlFor="message">Message</label>
                 </motion.div>
-                <motion.button variants={SECTION_VARIANTS.fadeUpElement} className="submit-button" type="submit" onClick={handleSubmit}>
+                <motion.button variants={SECTION_VARIANTS.fadeUpElement} className="submit-button" type="submit" >
                     Submit
                     <svg className="submit-arrow" xmlns="http://www.w3.org/2000/svg" width="18.752" height="18.277" viewBox="0 0 18.752 18.277">
                         <path fill="currentColor" id="Icon_awesome-arrow-right" data-name="Icon awesome-arrow-right" d="M7.973,3.871,8.9,2.942a1,1,0,0,1,1.419,0l8.136,8.132a1,1,0,0,1,0,1.419l-8.136,8.136a1,1,0,0,1-1.419,0L7.973,19.7a1.006,1.006,0,0,1,.017-1.436l5.043-4.8H1a1,1,0,0,1-1-1V11.116a1,1,0,0,1,1-1H13.033L7.99,5.307A1,1,0,0,1,7.973,3.871Z" transform="translate(0 -2.647)" />
